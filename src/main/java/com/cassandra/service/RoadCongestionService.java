f5fe0099800f2863b461707a5079b6317ef059b4
mmpsj <jr1504@konkuk.ac.kr>
2024-06-08 23:16:21 +0900

    feat: 5분마다 cassandra에서 데이터 읽어와 저장 구현

--- src/main/java/com/cassandra/service/RoadCongestionService.java
+++ src/main/java/com/cassandra/service/RoadCongestionService.java
@@ -4,24 +4,32 @@ import com.cassandra.domain.Road;
 import com.cassandra.repository.RoadRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Slf4j
 @Service
 public class RoadCongestionService {
 
-    private RoadRepository roadRepository;
+    private final RoadRepository roadRepository;
+    private List<Road> roadList;
 
     @Autowired
     public RoadCongestionService(RoadRepository roadRepository) {
         this.roadRepository = roadRepository;
     }
 
-    public Optional<String> getCongestionLevelByRoadName(String roadName) {
-        log.info("Requested roadname = {}", roadName);
-        return roadRepository.findByRoadName(roadName)
-                .map(Road::getCongestionLevel);
+    @Scheduled(fixedRate = 300000)
+    public void fetchTrafficData() {
+        roadList = roadRepository.findAll();
+        roadList.forEach(info -> {
+            log.info("도로명: {}, 혼잡도: {}", info.getRoadName(), info.getCongestionLevel());
+        });
+    }
+
+    public List<Road> getAllCongestionLevel() {
+        return roadList;
     }
 }
